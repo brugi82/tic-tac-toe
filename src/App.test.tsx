c2b242dc0e12852a1f2605817209714f7f505347
Milos Petrovic <milos.brugi@gmail.com>
2019-04-29 21:57:16 -0400

    Adding tests for Routing.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router';
+import { shallow, mount, ReactWrapper } from 'enzyme';
+import GamePage from './components/game/GamePage';
 import App from './App';
+// setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import HistoryPage from './components/history/HistoryPage';
+import NotFoundPage from './components/common/NotFoundPage';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('Routes to Game component on default route.', () => {
+    const component: ReactWrapper = mount( 
+      <MemoryRouter initialEntries = {['/']} >
+        <App/>
+      </MemoryRouter>
+    );
+    expect(component.find(GamePage)).toHaveLength(1);
+  });
+
+  it('Routes to History component on history route.', () => {
+    const component: ReactWrapper = mount( 
+      <MemoryRouter initialEntries = {['/history']} >
+        <App/>
+      </MemoryRouter>
+    );
+    expect(component.find(HistoryPage)).toHaveLength(1);
+  });
+
+  it('Routes to NotFoundPage component when route not found.', () => {
+    const component: ReactWrapper = mount( 
+      <MemoryRouter initialEntries = {['/sdfsdf']} >
+        <App/>
+      </MemoryRouter>
+    );
+    expect(component.find(NotFoundPage)).toHaveLength(1);
+  });
 });
+
